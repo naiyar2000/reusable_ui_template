9a4d613b2b4abfdb6839741de24f1ef968b6e9a2
naiyar2000 <naiyarimam.2000@gmail.com>
2025-08-01 12:00:00 +0530

    removed unused imports

--- apps/web/app/(protected)/profile/page.tsx
+++ apps/web/app/(protected)/profile/page.tsx
@@ -3,7 +3,7 @@ import Editor from '@/app/component/Editor'
 
 const ProfilePage = () => {
   return (
-    <div className="h-screen flex-1 flex flex-col p-4"> {/* Adjust height if needed */}
+    <div className="h-screen flex-1 flex flex-col p-4">
       <div className="flex-1 border border-border rounded-md overflow-y-auto py-6">
         <Editor />
       </div>
